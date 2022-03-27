2c3ec2627f795994d8a85a678f991fdf402f249c
SU_MIN_JING <goodcack@gmail.com>
2022-03-27 20:14:10 +0800

    roughly finish Q4 and Q5

--- week1/src/main/kotlin/Main.kt
+++ week1/src/main/kotlin/Main.kt
@@ -1,7 +1,25 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+open class Human(open val name: String) {
+    open fun attack() {
+        println("$name use Fist Attack!")
+    }
 }
+//${name}的錯誤: Don't use curly braces when inserting a simple variable into a string template. Use curly braces only for longer expressions.
+//在字串範本中插入簡單變數不要使用大括號，大括號，即Lambda表達式，僅在較長運算式使用。
+//Property 'name' could be private。
+//val name可以private ?
+
+class Mage(name: String) : Human(name) {
+    override fun attack() {
+        println("$name use Fireball!")
+    }
+}
+//因為子類別Mage要從超類別Human繼承name的String宣告，name 需為public，不需在超類別private name。
+
+fun main() {
+    Human("Jack").attack()
+    Mage("Rose").attack()
+}
+//變數HumanA的錯誤: Local variable name 'HumanA' should start with a lowercase letter.
+//局部變數建議以小寫首字母命名。
+//Names of functions, properties and local variables start with a lowercase letter and use camel case and no underscores.
+//函數、屬性、局部變數的名稱使用駝峰式大小寫、無下底線。
