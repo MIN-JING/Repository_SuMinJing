3ab20dea19aaf88b5e64c2a94966894023063397
SU_MIN_JING <goodcack@gmail.com>
2022-04-03 21:29:27 +0800

    Add a Menu(Uppon-right) for ProfileFragment

--- week2/app/src/main/java/com/example/android/navigation/ProfileFragment.kt
+++ week2/app/src/main/java/com/example/android/navigation/ProfileFragment.kt
@@ -1,12 +1,11 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-//import androidx.databinding.DataBindingUtil
-//import com.example.android.navigation.databinding.FragmentTitleBinding
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
 
 class ProfileFragment : Fragment() {
 
@@ -14,5 +13,15 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                          savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+            view!!.findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
